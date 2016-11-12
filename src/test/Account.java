753b31fd28dc0673577b73bf3860cce88901e3be
sunwook <coolutony@gmail.com>
2016-11-12 16:32:14 -0500

    Account method added; Transfer added

--- src/test/Account.java
+++ src/test/Account.java
@@ -1,6 +1,17 @@
 package test;
 
-	// TODO: Fields here 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.*;
+import java.util.ArrayList;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.*;
+
+import test.Account.Type;
+
 public class Account {
 	
 	public Account(String sa, int b, String c, String n, int r, Type t){
@@ -37,6 +48,54 @@ public class Account {
 	}
 	public Type getType(){
 		return type;
+	}
+	public static ArrayList<Account> createList(URL url) throws IOException{
+		URLConnection myURLConnection = url.openConnection();
+		try {
+			myURLConnection.connect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+		String jsonText = readAll(in);
+		JSONObject json = new JSONObject(jsonText);
+		return createList(json);
+	}
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+	public static ArrayList<Account> createList(JSONObject json){
+		JSONArray ja = json.getJSONArray("results");
+		ArrayList<Account> a = new ArrayList<>();
+		for (int i = 0; i < ja.length(); i++) {
+			JSONObject temp = ja.getJSONObject(i);
+			String id = temp.getString("_id");
+			int bal = temp.getInt("balance");
+			String c_id = temp.getString("customer_id");
+			String nickname = temp.getString("nickname");
+			int rewards = temp.getInt("rewards");
+			String type = temp.getString("type");
+			Type t = null;
+			switch (type) {
+			case "savings":
+				t = Type.SAVINGS;
+				break;
+			case "checking":
+				t = Type.CHECKING;
+				break;
+			case "credit_card":
+				t = Type.CREDIT_CARD;
+				break;
+			}
+
+			a.add(new Account(id, bal, c_id, nickname, rewards, t));
+		}
+		return a;
 	}
 	
 }
