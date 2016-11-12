4c46fea225a16b56605dbf57ef9ecbe507ebbd4f
sunwook <coolutony@gmail.com>
2016-11-12 15:49:11 -0500

    Account

--- src/test/Account.java
+++ src/test/Account.java
@@ -1,5 +1,12 @@
 package test;
 
 public class Account {
-
+	private String accountId;
+	private int balance;
+	private String customerId;
+	private String nickname;
+	private int rewards;
+	private Type type; 
+	public enum Type {CREDIT_CARD,SAVINGS,CHECKING};
+	
 }
