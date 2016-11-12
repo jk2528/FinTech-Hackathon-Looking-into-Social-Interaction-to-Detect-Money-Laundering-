38e5ba4929ed1a47784c6a7a4d197a5053bafc04
Jinju Ouck <Jinju Ouck>
2016-11-12 14:53:27 -0500

    url

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,31 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Test {
-	public static void main(String args[]){
-		System.out.println("This is a test");
+	public static void main(String args[]) {
+		try {
+			URL myURL = new URL(
+					"http://api.reimaginebanking.com/enterprise/accounts?key=358d729f3e85f5d73acd2790b0ae32b6");
+			URLConnection myURLConnection = myURL.openConnection();
+			myURLConnection.connect();
+			BufferedReader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+		} catch (MalformedURLException e) {
+			// new URL() failed
+			// ...
+		} catch (IOException e) {
+			// openConnection() failed
+			// ...
+		}
 	}
 
 }
