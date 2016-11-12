a985822b5e95d987f20a0a986563067dd5ba07d3
sunwook <coolutony@gmail.com>
2016-11-12 17:12:52 -0500

    Transfer Method Added

--- src/test/Transfer.java
+++ src/test/Transfer.java
@@ -1,5 +1,18 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import test.Account.Type;
+
 public class Transfer {
 	private String transferId;
 	private int amount;
@@ -22,5 +35,47 @@ public class Transfer {
 		month = m;
 		this.day = day;
 	}
+	
+	public static ArrayList<Transfer> createList(URL url) throws IOException{
+		URLConnection myURLConnection = url.openConnection();
+		try {
+			myURLConnection.connect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+		String jsonText = readAll(in);
+		JSONObject json = new JSONObject(jsonText);
+		return createList(json);
+	}
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+	
+	public static ArrayList<Transfer> createList(JSONObject json){
+		JSONArray ja = json.getJSONArray("results");
+		ArrayList<Transfer> a = new ArrayList<>();
+		for (int i = 0; i < ja.length(); i++) {
+			JSONObject temp = ja.getJSONObject(i);
+			String id = temp.getString("_id");
+			int am = temp.getInt("amount");
+			String dis = temp.getString("description");
+			String payee = temp.getString("payee_id");
+			String payer = temp.getString("payer_id");
+			boolean ex = ("executed"== temp.getString("status"));
+			String date = temp.getString("transaction_date");
+			int y = Integer.parseInt(date.substring(0, 3));
+			int m = Integer.parseInt(date.substring(5,6));
+			int day = Integer.parseInt(date.substring(8,9));
+			a.add(new Transfer(id,am,dis,payee,payer,ex,y,m,day));
+		}
+		return a;
+	}
+	
 	
 }
