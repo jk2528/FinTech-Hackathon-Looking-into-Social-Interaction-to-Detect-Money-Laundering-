8b2b49ee759a146dddc68ea1c1da6c092b9d145e
sunwook <coolutony@gmail.com>
2016-11-12 15:56:40 -0500

    Account Getter Added

--- src/test/Account.java
+++ src/test/Account.java
@@ -9,4 +9,24 @@ public class Account {
 	private Type type; 
 	public enum Type {CREDIT_CARD,SAVINGS,CHECKING};
 	
+	
+	public String getAccountId(){
+		return accountId;
+	}
+	public int getBalance(){
+		return balance;
+	}
+	public String getCustomerId(){
+		return customerId;
+	}
+	public String getNickname(){
+		return nickname;
+	}
+	public int getRewards(){
+		return rewards;
+	}
+	public Type getType(){
+		return type;
+	}
+	
 }
