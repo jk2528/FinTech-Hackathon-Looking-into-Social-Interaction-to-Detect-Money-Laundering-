9e5f34d655d91764a87df00820cb1b3748c50337
Jinju Ouck <Jinju Ouck>
2016-11-12 21:16:00 -0500

    djsfl

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,4 +1,5 @@
 package test;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,25 +19,33 @@ import test.Account.Type;
 public class Test {
 	public static void main(String args[]) {
 		try {
-			URL myURL = new URL(
+			URL customerURL = new URL(
+					"http://api.reimaginebanking.com/enterprise/customers?key=358d729f3e85f5d73acd2790b0ae32b6");
+			URL accountURL = new URL(
 					"http://api.reimaginebanking.com/enterprise/accounts?key=358d729f3e85f5d73acd2790b0ae32b6");
-			URLConnection myURLConnection = myURL.openConnection();
+			URL transferURL = new URL(
+					"http://api.reimaginebanking.com/enterprise/transfers?key=358d729f3e85f5d73acd2790b0ae32b6");
+
+			URLConnection myURLConnection = accountURL.openConnection();
 			myURLConnection.connect();
 			BufferedReader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
 			String jsonText = readAll(in);
 			JSONObject json = new JSONObject(jsonText);
+
+			
+			HashMap<String, Customer> customers = Customer.createMap(customerURL);
 			HashMap<String, Account> a = Account.createMap(json);
-			ArrayList<Transfer> b = Transfer.createList(
-					new URL("http://api.reimaginebanking.com/enterprise/transfers?key=358d729f3e85f5d73acd2790b0ae32b6"),
-					a);
+			ArrayList<Transfer> b = Transfer.createList(transferURL, a);
+
+			
 			
 			/*
-			for (Object x : a.keySet().toArray()) {
-				System.out.println(((Account) (a.get(x))).getNickname());
+			 * for (Object x : a.keySet().toArray()) {
+			 * System.out.println(((Account) (a.get(x))).getNickname());
+			 * 
+			 * }
+			 */
 
-			}
-			*/
-			
 			for (Transfer x : b) {
 				System.out.println(x.getDescription());
 
